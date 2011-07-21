a8cdf0af80b3f44e340d04f3600c5069f7193259
lessofme.patrick@gmail.com <lessofme.patrick@gmail.com@434479da-d0d6-a7c0-be0c-54d28c6a25bb>
2011-07-21 19:53:47 +0000

    Revised notes in TextParser.java to be more clear, and relevant to current operation. PK

--- trunk/ android-word-search/WordSearch/src/pk/dl/jk/wordsearch/TextParser.java
+++ trunk/ android-word-search/WordSearch/src/pk/dl/jk/wordsearch/TextParser.java
@@ -22,25 +22,20 @@ public class TextParser extends Activity {
 	 * This method, getWords(), will return an ArrayList of words.
 	 * 	The words returned will be based off of the list it is told
 	 * 	to choose from, and the number of words it is told to return.
+	 * 
+	 *  Now able to scan text file, and return 10 words in alphabetical order. 
+	 *  Words are <=10 characters long, to make sure they fit in our board. PK
 	 */
 	public ArrayList<String> getWords(int list, int numWords, String wordListText) throws IOException
 	{
 		ArrayList<String> wordList = new ArrayList<String>();
-		//wordList.add(wordListText);
 		
+		/*
+		 *  Scanner will parse full String of our text file into an ArrayList, by word.
+		 */
 		Scanner scan = new Scanner(wordListText);
 		Integer listWords = 0;
 		
-		/*
-		 * Create Randomizer to help parse through the words
-		 *	(should take into account total number of words in list,
-		 * 	and how many words are needed). Assuming file is in alphabetical
-		 * 	order, make sure we can fill the order without stepping over
-		 * 	the bounds of the list. This will help keep things nice and clean,
-		 *	without having to re-alphabetize later. 
-		 */
-		
-		// °loop here. -_- 
 		String chosenWord = "";
 		while (scan.hasNextLine() && !chosenWord.matches("END"))
 		{
@@ -50,6 +45,7 @@ public class TextParser extends Activity {
 				wordList.add(chosenWord);
 			listWords++;
 		}
+		
 		//full list of words
 		wordList.remove(wordList.indexOf("END"));
 		wordList.trimToSize();
@@ -58,6 +54,7 @@ public class TextParser extends Activity {
 		Random rand = new Random();
 		int temp = rand.nextInt(wordList.size()/10);
 		
+		// Grabbing ten words, in order, pseudo-randomly.
 		for(int i = 0; i < numWords; i++)
 		{
 			finalWordList.add(wordList.get(temp));
@@ -66,7 +63,6 @@ public class TextParser extends Activity {
 		
 		finalWordList.trimToSize();
 		
-		// Use loop to parse through file, adding words to the ArrayList.
 		return finalWordList;		
 	}
 
